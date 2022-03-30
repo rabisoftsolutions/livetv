a1897803f30678b130c3e9ab953e4f38a4ab0cd8
rabindra <rabindramarndi1@gmail.com>
2022-03-30 07:31:53 +0530

    initial commit

---
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -0,0 +1,224 @@
+package com.example.videoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    PlayerView playerView;
+    ImageView fullScreen;
+    boolean isFullScreen = false;
+    SimpleExoPlayer player;
+    ProgressBar progressBar;
+    private boolean isShowingTrackSelectionDialog;
+    private DefaultTrackSelector trackSelector;
+
+    String[] speed = {"0.25x","0.5x","Normal","1.5x","2x"};
+    String live_url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        trackSelector = new DefaultTrackSelector(this);
+        player = new SimpleExoPlayer.Builder(this).setTrackSelector(trackSelector).build();
+        playerView = findViewById(R.id.playerView);
+        playerView.setPlayer(player);
+        MediaItem mediaItem = MediaItem.fromUri(live_url);
+        player.setMediaItem(mediaItem);
+        player.prepare();
+        player.setPlayWhenReady(true);
+
+
+        TextView speedTxt = playerView.findViewById(R.id.speed);
+        playerView = findViewById(R.id.playerView);
+        fullScreen = playerView.findViewById(R.id.exo_fullscreen_button);
+        ImageView speedBtn = playerView.findViewById(R.id.exo_playback_speed);
+        progressBar = findViewById(R.id.progressBar);
+
+        speedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Set Speed");
+                builder.setItems(speed, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // the user clicked on colors[which]
+
+                        if (which==0){
+
+                            speedTxt.setVisibility(View.VISIBLE);
+                            speedTxt.setText("0.25X");
+                            PlaybackParameters param = new PlaybackParameters(0.5f);
+                            player.setPlaybackParameters(param);
+
+
+                        }  if (which==1){
+
+                            speedTxt.setVisibility(View.VISIBLE);
+                            speedTxt.setText("0.5X");
+                            PlaybackParameters param = new PlaybackParameters(0.5f);
+                            player.setPlaybackParameters(param);
+
+
+                        }
+                        if (which==2){
+
+                            speedTxt.setVisibility(View.GONE);
+                            PlaybackParameters param = new PlaybackParameters(1f);
+                            player.setPlaybackParameters(param);
+
+
+                        }
+                        if (which==3){
+                            speedTxt.setVisibility(View.VISIBLE);
+                            speedTxt.setText("1.5X");
+                            PlaybackParameters param = new PlaybackParameters(1.5f);
+                            player.setPlaybackParameters(param);
+
+                        }
+                        if (which==4){
+
+
+                            speedTxt.setVisibility(View.VISIBLE);
+                            speedTxt.setText("2X");
+
+                            PlaybackParameters param = new PlaybackParameters(2f);
+                            player.setPlaybackParameters(param);
+
+
+
+                        }
+
+
+
+                    }
+                });
+                builder.show();
+
+
+
+
+
+            }
+        });
+
+        fullScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFullScreen) {
+
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+
+                    if (getSupportActionBar() != null) {
+                        getSupportActionBar().show();
+                    }
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+                    ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) playerView.getLayoutParams();
+                    params.width = params.MATCH_PARENT;
+                    params.height = (int) (200 * getApplicationContext().getResources().getDisplayMetrics().density);
+                    playerView.setLayoutParams(params);
+
+//                    Toast.makeText(Details.this, "We are Now going back to normal mode.", Toast.LENGTH_SHORT).show();
+                    isFullScreen = false;
+                } else {
+
+                    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+                    if (getSupportActionBar() != null) {
+                        getSupportActionBar().hide();
+                    }
+
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                    ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) playerView.getLayoutParams();
+                    params.width = params.MATCH_PARENT;
+                    params.height = params.MATCH_PARENT;
+                    playerView.setLayoutParams(params);
+
+//                    Toast.makeText(Details.this, "We are going to FullScreen Mode.", Toast.LENGTH_SHORT).show();
+                    isFullScreen = true;
+                }
+            }
+        });
+        playerView = findViewById(R.id.playerView);
+        ImageView setting = playerView.findViewById(R.id.exo_track_selection_view);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isShowingTrackSelectionDialog
+                        && TrackSelectionDialog.willHaveContent(trackSelector)) {
+                    isShowingTrackSelectionDialog = true;
+                    TrackSelectionDialog trackSelectionDialog =
+                            TrackSelectionDialog.createForTrackSelector(
+                                    trackSelector,
+                                    /* onDismissListener= */ dismissedDialog -> isShowingTrackSelectionDialog = false);
+                    trackSelectionDialog.show(getSupportFragmentManager(), /* tag= */ null);
+
+
+                }
+
+
+            }
+        });
+
+        player.addListener(new Player.Listener() {
+            @Override
+            public void onPlaybackStateChanged(int state) {
+                if (state == Player.STATE_READY) {
+                    progressBar.setVisibility(View.GONE);
+                    player.setPlayWhenReady(true);
+                } else if (state == Player.STATE_BUFFERING) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    playerView.setKeepScreenOn(true);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    player.setPlayWhenReady(true);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        player.seekToDefaultPosition();
+        player.setPlayWhenReady(true);
+    }
+
+    @Override
+    protected void onPause() {
+        player.setPlayWhenReady(false);
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        player.release();
+        super.onDestroy();
+    }
+}
